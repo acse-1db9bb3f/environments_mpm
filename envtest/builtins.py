db686ce2b8a40cbbf8c057f6089b768b20245fcb
acse-1db9bb3f <wz22@ic.ac.uk>
2022-10-10 15:51:02 +0100

    <my commit message>

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,9 +1,9 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
-
-__all__ = ['rand_array','smooth_image','my_mat_solve']
+__all__ = ['rand_array','smooth_image','my_mat_solve', 'date']
 
 def smooth_image(a, sigma=1):
     return gaussian_filter(a, sigma=sigma)
@@ -13,3 +13,6 @@ def rand_array(shape):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def date(_d, _p):
+    return pd.date_range(_d, periods=_p)
---
+++ scripts/date.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from envtest import date 
+
+d = "20221010"
+p = 6
+
+dates = date(d, p)
+print(dates)
